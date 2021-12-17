6af59a06e47af2799d2b78a206be1bf2c403c1e4
Deni-Prasetyo <prasetyodenibudi@gmail.com>
2021-12-17 21:06:10 +0700

    create table

---
+++ src/pages/dashboard/index.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { Button, Row, Col, Table } from "reactstrap";
+import Modal from "../../component/Modal";
+import jsonData from "../../db/product.json";
+import FormCreate from "./createData";
+import FormEdit from "./editData";
+
+const Dashboard = () => {
+  const [data, setData] = useState({ headers: [], rows: [] });
+  useEffect(() => {
+    setData(jsonData);
+  }, []);
+
+  console.log(data);
+  return (
+    <>
+      <h1>CRUD Data</h1>
+      <br />
+      <Table>
+        <thead>
+          <tr>
+            {data.headers.map((header, idx) => (
+              <th key={idx}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+      </Table>
+    </>
+  );
+};
+
+export default Dashboard;
