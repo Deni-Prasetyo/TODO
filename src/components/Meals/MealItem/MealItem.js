828f7a10d6d203bb7e286264dd194f26c5ba084c
Deni-Prasetyo <prasetyodenibudi@gmail.com>
2022-01-01 21:38:03 +0700

    Add button to input Order

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,13 +1,17 @@
+import MealItemForm from "./MealItemForm";
 import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
   const price = `Rp ${props.price.toFixed(3)}`;
   return (
-    <li>
+    <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm />
       </div>
     </li>
   );
