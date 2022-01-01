ab96058ac5fae5726bfec5b42a89d5f714183f26
Deni-Prasetyo <prasetyodenibudi@gmail.com>
2022-01-01 20:53:00 +0700

    Add Card Product

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,11 +1,11 @@
-import classes from "./Mealssummary.module.css";
+import classes from "./MealsSummary.module.css";
 
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
       <h2>Dari Indonesia Untuk Anda</h2>
       <p>Pilih makanan favorit Anda, nikmati makan siang atau makan malam lezat cukup dari rumah.</p>
-      <p>Semua makanan kami terbuat dari bahan-bahan berkualitas, dan tentunya dibuat oleh koki berpengalaman.</p>
+      <h4>Semua makanan kami terbuat dari bahan-bahan berkualitas, dan tentunya dibuat oleh koki berpengalaman.</h4>
     </section>
   );
 };
